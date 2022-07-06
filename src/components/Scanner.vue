6e649ea7fe2520250e5890635d17d4f8cde37fce
petr <petyalom@yandex.ru>
2022-07-06 21:16:30 +0300

    update

--- src/components/Scanner.vue
+++ src/components/Scanner.vue
@@ -30,9 +30,9 @@ export default {
 
   data() {
     return {
-      scanResult: String,
-      error: String,
-      text: String,
+      scanResult: '',
+      error: '',
+      text: '',
     }
   },
 
